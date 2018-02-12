d121a888bace304033583e72fe521c07e3acc668
Wenjie <traxiaojie@gmail.com>
2018-02-12 15:19:10 -0800

    Created rest of the necessary class methods

--- word_guess.rb
+++ word_guess.rb
@@ -22,24 +22,48 @@ class WordGuess
   end
 
   def check_guess(user_input)
-    @word_array.each_with_index do |letter, i|
-      if user_input == letter
-        @display_array[i] = letter
+    if @word_array.include?(user_input)
+      @word_array.each_with_index do |letter, i|
+        if user_input == letter
+          @display_array[i] = letter
+        end
       end
-
+    else
+      @flowers.delete_at(-1)
     end
 
   end
+
+  def won?
+    if @display_array == @word_array
+      return true
+    else
+      return false
+    end
+  end
+
+  def lost?
+    if @flowers.length == 0
+      return true
+    else
+      return false
+    end
+
+  end
+
+
+
 end
 
 test = WordGuess.new("sitting")
-print test.print_display
+puts test.print_display
+puts test.print_picture
 
+puts "Guess a letter:"
 input = gets.chomp
 test.check_guess(input)
 puts test.print_display
-
-
+puts test.print_picture
 
 
 
