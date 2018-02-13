773eda488ae74f8fa2ce0d38ec9e206c0b185556
Wenjie <traxiaojie@gmail.com>
2018-02-12 16:37:32 -0800

    Created interface loop and counted for win and lose. Currently accept single letter. @4:30pm

--- word_guess.rb
+++ word_guess.rb
@@ -7,6 +7,7 @@ class WordGuess
     @word_array = word.split("")
     @display_array = Array.new(word.length, "_")
     @flowers = Array.new(5, "<3")
+    @user_guesses = []
   end
 
   def print_display
@@ -31,7 +32,15 @@ class WordGuess
     else
       @flowers.delete_at(-1)
     end
+    @user_guesses << user_input
 
+    if won?
+      puts "YOU WIN! Congratulations!"
+    end
+
+    if lost?
+      puts "YOU LOSE. Sorry :("
+    end
   end
 
   def won?
@@ -48,22 +57,89 @@ class WordGuess
     else
       return false
     end
+  end
 
+  def user_guesses
+    return @user_guesses
   end
 
 
 
 end
 
-test = WordGuess.new("sitting")
-puts test.print_display
-puts test.print_picture
+# guesses = []
+#
+# test = WordGuess.new("sitting")
+# puts test.print_display
+# puts test.print_picture
+#
+# puts "Guess a letter:"
+# input = gets.chomp
+# test.check_guess(input)
+# puts test.print_display
+# puts test.print_picture
+# puts test.won?
+# puts test.lost?
+#
 
-puts "Guess a letter:"
-input = gets.chomp
-test.check_guess(input)
-puts test.print_display
-puts test.print_picture
+
+def update(game)
+  puts "Status".center(20,"=")
+  if !(game.won?) && (!game.lost?)
+    puts game.print_display
+    puts game.print_picture
+    puts game.user_guesses
+  end
+
+end
+
+def is_a_letter?(input)
+  input =~ /[a-z]/
+end
+
+word_bank = %W[watermelon taco seattle]
+
+puts "Welcome to the Flower Word Game!"
+puts "\nDirections: Guess one letter at a time to try to solve the word puzzle. You will see an updated display after each turn. If you run out of hearts, you lose the game! Good luck!!!\n"
+
+flag = true
+while flag
+  new_game = WordGuess.new(word_bank.sample)
+  update(new_game)
+  until new_game.won? || new_game.lost?
+    puts "Guess a letter: "
+    guess = gets.chomp.downcase
+
+    until guess.length == 1 && is_a_letter?(guess) && !new_game.user_guesses.include?(guess)
+      if new_game.user_guesses.include?(guess)
+        puts "You've already tried #{guess}. Please enter another letter: "
+      else
+        puts "Sorry! Please enter only one letter: "
+      end
+      guess = gets.chomp.downcase
+
+    end
+
+
+
+    puts "=======checking======"
+    new_game.check_guess(guess)
+    update(new_game)
+
+  end
+
+  puts "Would you like to play another game? (yes/no)"
+  user_decision = gets.chomp.downcase
+  if user_decision == "yes"
+    flag = true
+  else
+    flag = false
+  end
+end
+
+puts "Thanks for playing our awesome word game!! :)"
+
+
 
 
 
